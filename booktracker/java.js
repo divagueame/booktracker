90be59744ba691fcf04f4156ff44ff7ad85efb46
diva <martin.arce.teixeira@gmail.com>
2020-04-10 00:45:43 +0200

    Vcode Commit

--- booktracker/java.js
+++ booktracker/java.js
@@ -71,3 +71,15 @@ db.collection('myLibrary').orderBy('title').onSnapshot(snapshot=>{
         
     })
 })
+
+
+// RANGE STYLING
+var elem = document.querySelector('input[type="range"]');
+
+var rangeValue = function(){
+  var newValue = elem.value;
+  var target = document.querySelector('.value');
+  target.innerHTML = newValue;
+}
+
+elem.addEventListener("input", rangeValue);
