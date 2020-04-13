567200e33867d9ecedee26549f3396afde321900
diva <martin.arce.teixeira@gmail.com>
2020-04-13 20:28:55 +0200

    New commit. Basic design complete

--- booktracker/java.js
+++ booktracker/java.js
@@ -1,24 +1,65 @@
 const bookList = document.querySelector("#book-list"); // GITLETS WORKS
 const addBookForm = document.querySelector('#add-book-form')
 
-function renderBook(book){
-    let li = document.createElement("li");
-    let title = document.createElement("span");
-    let author = document.createElement("span");
-    let rating = document.createElement("span");
-    let cross = document.createElement("div");
+// function renderBook(book){
+//     let li = document.createElement("li");
+//     let title = document.createElement("span");
+//     let author = document.createElement("span");
+//     let rating = document.createElement("span");
+//     let cross = document.createElement("div");
     
-    li.setAttribute('data-id', book.id);
+//     li.setAttribute('data-id', book.id);
+//     title.textContent = book.data().title;
+//     author.textContent = book.data().author;
+//     rating.textContent = book.data().rating;
+//     cross.textContent = 'x';
+
+//     li.appendChild(title);
+//     li.appendChild(author);
+//     li.appendChild(rating);
+//     li.appendChild(cross)
+//     bookList.appendChild(li);
+
+// //DELETING DATA WITH THE CROSS
+//     cross.addEventListener('click',(e)=>{
+//         e.stopPropagation();
+//         let id = e.target.parentElement.getAttribute('data-id');
+//         db.collection('myLibrary').doc(id).delete()
+//     })
+
+// }
+
+
+//New Version of renderBook
+function renderBook(book){
+    let bookEntry = document.createElement("div");
+    let title = document.createElement("div");
+    let author = document.createElement("div");
+    let rating = document.createElement("div");
+    let cross = document.createElement("div");
+
+
+    bookEntry.setAttribute('data-id', book.id);
     title.textContent = book.data().title;
     author.textContent = book.data().author;
     rating.textContent = book.data().rating;
     cross.textContent = 'x';
+    title.classList.add("title")
+    
+    author.classList.add("author")
+    rating.classList.add("rating")
+    cross.classList.add("cross")
+    title.classList.add("entryData")
+    author.classList.add("entryData")
+    rating.classList.add("entryData")
+    cross.classList.add("entryData")
 
-    li.appendChild(title);
-    li.appendChild(author);
-    li.appendChild(rating);
-    li.appendChild(cross)
-    bookList.appendChild(li);
+
+    bookEntry.appendChild(title);
+    bookEntry.appendChild(author);
+    bookEntry.appendChild(rating);
+    bookEntry.appendChild(cross)
+    bookList.appendChild(bookEntry);
 
 //DELETING DATA WITH THE CROSS
     cross.addEventListener('click',(e)=>{
@@ -28,6 +69,8 @@ function renderBook(book){
     })
 
 }
+
+
 
 // GETTING DATA
 // db.collection('myLibrary').get().then((snapshot)=> {
@@ -60,8 +103,8 @@ db.collection('myLibrary').orderBy('title').onSnapshot(snapshot=>{
         if(change.type == 'added'){
             renderBook(change.doc)
         } else if(change.type = 'removed'){
-            let li = bookList.querySelector('[data-id='+change.doc.id+']');
-            bookList.removeChild(li);
+            let bookEntry = bookList.querySelector('[data-id='+change.doc.id+']');
+            bookList.removeChild(bookEntry);
         }
         
         
